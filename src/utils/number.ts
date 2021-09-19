7d91f1f9e246b9575e8984c87b9e64591412c8ed
robsonnjunnior <robsonn.junnior@gmail.com>
2021-09-18 22:08:11 -0300

    fix util porcentagem

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -7,6 +7,6 @@ export const currencyFormat = (value = 0) => {
 };
 
 export const percentFormat = (value = 0, decimals = 2) => {
-  const formatted = `${value.toFixed(decimals).replace(/0+$/, '')}%`;
+  const formatted = `${+value.toFixed(decimals).replace(/0+$/, '')}%`;
   return formatted;
 };
